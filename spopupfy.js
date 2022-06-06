f7934ca304464e059cbffc348f6cc2e979ce1e1d
Anghelos Coulon <angheloc@gmail.com>
2022-06-06 13:05:37 -0400

    Multiple improvements

--- spopupfy.js
+++ spopupfy.js
@@ -1,23 +1,31 @@
+const expandSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 11" fill="currentColor">
+<rect x=".75" y=".75" width="14.5" height="9.5" rx="2.7" ry="2.7" fill="none" stroke="currentColor" stroke-width="1.5"/>
+<rect x="2.6" y="2.6" width="10.8" height="5.8" rx=".4" ry=".4"/>
+</svg>`;
+
+const popupSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 11" fill="currentColor">
+                    <rect x=".75" y=".75" width="14.5" height="9.5" rx="2.7" ry="2.7" fill="none" stroke="currentColor" stroke-width="1.5"/>
+                    <rect class="inner" x="2.6" y="2.6" width="5" height="2.5" rx=".4" ry=".4"/>
+                  </svg>`;
 
 function miniplayer() {
   document.body.classList.toggle('spopupfy');
   if (document.body.classList.contains('spopupfy')) {
     chrome.runtime.sendMessage({ text: "spopupfy" });
+    // this.innerHTML = expandSVG;
   }
   else {
     chrome.runtime.sendMessage({ text: "backToSpotify" });
+    // this.innerHTML = popupSVG;
   }
 }
 
 const addButton = function () {
-  let button = document.querySelector('.spopupfy-button');
+  let button = document.getElementById('spopupfy-button');
   if (!button) {
     button = document.createElement('button');
-    button.classList.add('spopupfy-button');
-    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-aspect-ratio" viewBox="0 0 16 16">
-  <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
-  <path d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0v-3zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0v3z"/>
-</svg>`;
+    button.id = 'spopupfy-button';
+    button.innerHTML = popupSVG;
 
     document.body.appendChild(button);
   }
